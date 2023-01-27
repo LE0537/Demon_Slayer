7a8f9bbada30cb7684d12bf3f362fcb85ef8dce4
석윤지 <34976092+Linchoa@users.noreply.github.com>
2023-01-27 21:57:05 +0900

    01.27 21:57

--- Client/Private/AkazaAtk_2_State.cpp
+++ Client/Private/AkazaAtk_2_State.cpp
@@ -239,11 +239,10 @@ CAkazaState * CAtk_2_State::Late_Tick(CAkaza* pAkaza, _float fTimeDelta)
 					m_pTarget->Set_Hp(-pAkaza->Get_PlayerInfo().iDmg);
 					m_pTarget->Take_Damage(0.3f,false);
 				}
-				_matrix vTagetWorld = m_pTarget->Get_Transform()->Get_WorldMatrix();
 
 				CEffect_Manager* pEffectManger = GET_INSTANCE(CEffect_Manager);
 
-				pEffectManger->Create_Effect(CEffect_Manager::EFF_HIT, vTagetWorld);
+				pEffectManger->Create_Effect(CEffect_Manager::EFF_HIT, m_pTarget);
 
 				RELEASE_INSTANCE(CEffect_Manager);
 
