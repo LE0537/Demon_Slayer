7a2131d8f349e86a2e7f77828301594b3fb41d89
KRsjHeo <106720685+KRsjHeo@users.noreply.github.com>
2023-02-20 02:46:36 +0900

    루이 리메이크 완료
    ㅁㄴㅇㄹ

--- Client/Public/RuiShoot.h
+++ Client/Public/RuiShoot.h
@@ -36,6 +36,8 @@ private:
 	_bool		   m_bHit = false;
 	_bool		   m_bEffectDead = false;
 
+	_float			m_fEffectCreateTime = 0.f;
+
 public:
 	static CRuiShoot* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
 	virtual CGameObject* Clone(void* pArg = nullptr);
